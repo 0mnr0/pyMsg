7ea0afd21bd886aa4595ef98a418a6de0e31bbb3
JustDev <savelijln0@gmail.com>
2024-11-28 09:29:16 +0400

    Merge bdOps.py into databases.py

--- databases.py
+++ databases.py
@@ -1,6 +1,24 @@
 from datetime import datetime
-from bdOps import *
+from pymongo import *
+import os
 
+UPLOAD_FOLDER = 'uploads'
+
+# Убедимся, что папка существует
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+
+client = MongoClient('mongodb://localhost:27017')
+db = client.pyMsg
+
+def createIndexIfNotExists(dbname):
+    if dbname in client.list_database_names():
+        pass
+    else:
+        global db
+        db = client[dbname]
+        return db
 
 def get_all(db, collection_name):
     # Получить все элементы в коллекции
